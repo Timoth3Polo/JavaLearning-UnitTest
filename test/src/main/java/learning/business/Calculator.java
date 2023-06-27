c4d877b3c85e171c58df6fc9022eb26d2eba42da
Timoth3Polo <timoth3.paul@gmail.com>
2023-06-27 23:25:44 +0200

    Init project Java Test Learning

---
+++ test/src/main/java/learning/business/Calculator.java
@@ -0,0 +1,16 @@
+package learning.business;
+
+public class Calculator {
+
+    public int addTwoInteger(int numberOne, int numberTwo) {
+        return numberOne + numberTwo;
+    }
+
+    public int substractTwoInteger(int numberOne, int numberTwo) {
+        return numberOne - numberTwo;
+    }
+
+    public int multiplyTwoInteger(int numberOne, int numberTwo) {
+        return numberOne * numberTwo;
+    }
+}
