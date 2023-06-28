58d01b5a919430678e0f6930ee26eb2d8b19cf92
Timoth3Polo <timoth3.paul@gmail.com>
2023-06-28 20:18:08 +0200

    update for sonar check

---
+++ test/src/test/java/learning/business/CalculatorTestWAssert.java
@@ -0,0 +1,71 @@
+package learning.business;
+
+import org.junit.jupiter.api.*;
+
+import java.text.MessageFormat;
+import java.time.Duration;
+import java.time.Instant;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTestWAssert {
+
+    private static Instant startedAt;
+
+    private Calculator calculatorUnderTest;
+
+    @BeforeEach
+    public void methodeAppeleeAvantChaqueTest() {
+        System.out.println("Appel avant chaque test");
+        calculatorUnderTest = new Calculator();
+    }
+
+    @AfterEach
+    public void methodeAppeleeApresChaqueTest() {
+        System.out.println("Appel après chaque test");
+        calculatorUnderTest = null;
+    }
+
+    @BeforeAll
+    public static void methodeAppeleeAvantTousLesTests() {
+        System.out.println("Appel avant tous les tests");
+        startedAt = Instant.now();
+
+    }
+
+    @AfterAll
+    public static void methodeAppeleeApresTousLesTests() {
+        System.out.println("Appel après tous les tests");
+        Instant endedAt = Instant.now();
+        long duration = Duration.between(startedAt, endedAt).toMillis();
+        System.out.println(MessageFormat.format("Durée des tests : {0} ms", duration));
+    }
+
+    @Test
+    public void should_return_12() {
+        //Arrange
+        Integer numberOne = 2;
+        Integer numberTwo = 10;
+        Calculator calculator = new Calculator();
+
+        //Act
+        int result = calculator.addTwoInteger(numberOne, numberTwo);
+
+        //Assert
+        assertThat(result).isEqualTo(12);
+    }
+
+    @Test
+    public void should_return_15() {
+        //Arrange
+        Integer numberOne = 5;
+        Integer numberTwo = 10;
+
+        //Act
+        int result = calculatorUnderTest.addTwoInteger(numberOne, numberTwo);
+
+        //Assert
+        assertThat(result).isEqualTo(15);
+    }
+}
